5349c0572e0845a4eb62d8a27b84f974c948c0e9
shubham <thekingshubh07@gmail.com>
2024-08-17 11:53:58 -0700

    env updated

---
+++ components/shared/ProfileHeader.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  accountId: string;
+  authUserId: string;
+  name: string;
+  imgUrl: string;
+  bio: string;
+  username: string;
+  type?: "User" | "Community";
+}
+
+export default async function ProfileHeader({
+  accountId,
+  authUserId,
+  name,
+  imgUrl,
+  bio,
+  username,
+  type,
+}: Props) {
+  return (
+    <div className="w-full flex flex-col justify-center">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className=" relative h-20 w-20 object-cover">
+            <Image
+              src={imgUrl}
+              alt="Profile Photo"
+              fill
+              className="rounded-full object-cover shadow-xl"
+            />
+          </div>
+          <div className="flex-1 ">
+            <h2 className="text-left text-heading3-bold text-light-1">
+              {name}
+            </h2>
+            <p className="text-base-medium text-gray-1">@{username}</p>
+          </div>
+        </div>
+      </div>
+      <p className=" mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
+      <div className="mt-12 h-0.5 w-full bg-dark-3" />
+    </div>
+  );
+}
